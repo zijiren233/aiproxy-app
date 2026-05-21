cb0a86ba99227f685f1369cfd305a61eeb97c0e1
zjy365 <3161362058@qq.com>
2026-05-21 17:03:37 +0800

    init

---
+++ types/app-config.d.ts
@@ -0,0 +1,17 @@
+export type AppConfigType = {
+  common: {
+    docUrl: string
+  }
+  auth: {
+    appTokenJwtKey: string
+    aiProxyBackendKey: string
+    accountServerTokenJwtKey: string
+  }
+  backend: {
+    aiproxy: string
+    aiproxyInternal: string
+    accountServer: string
+  }
+  adminNameSpace: string[]
+  currencySymbol: 'shellCoin' | 'cny' | 'usd'
+}
